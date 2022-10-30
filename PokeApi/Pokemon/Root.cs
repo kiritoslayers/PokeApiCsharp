c3da8c804885fc669d6ada0091a101f2ffe1d2a5
Kirito <fersilvavillamarin@gmail.com>
2022-10-30 18:35:01 -0300

    Enviando projeto

---
+++ PokeApi/Pokemon/Root.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PokeApi.Pokemon
+{
+    public class Root
+    {
+        public int base_experience { get; set; }
+        public int height { get; set; }
+        //public List<object> held_items { get; set; }
+        public int id { get; set; }
+        public bool is_default { get; set; }
+        public string location_area_encounters { get; set; }
+        public string name { get; set; }
+        public int order { get; set; }
+        //public List<object> past_types { get; set; }
+        //public List<Type> types { get; set; }
+        public int weight { get; set; }
+    }
+}
---
+++ PokeApi/Program.cs
@@ -0,0 +1,76 @@
+﻿using Newtonsoft.Json;
+using PokeApi.Pokemon;
+using System;
+using System.Net;
+using System.Text.Json;
+
+namespace PokeApi
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Digite o número ou nome do pokémon: ");
+            string entrada = Console.ReadLine();
+
+            HttpWebRequest req = (HttpWebRequest)WebRequest.Create($"https://pokeapi.co/api/v2/pokemon/{entrada}");
+            HttpWebResponse response = (HttpWebResponse)req.GetResponse();
+            HttpStatusCode status = response.StatusCode;
+
+            string statusResponse = status.ToString();
+
+            if (status == HttpStatusCode.OK)
+            {
+                Stream dataStream = response.GetResponseStream();
+                StreamReader reader = new StreamReader(dataStream);
+                string responseFromServer = reader.ReadToEnd();
+
+                Root pokemon = JsonConvert.DeserializeObject<Root>(responseFromServer);
+
+                Console.WriteLine("");
+                Console.Write($"Número do pokemon: {pokemon.id}");
+                Console.WriteLine("");
+                Console.Write($"Nome do pokemon: {pokemon.name}");
+                Console.WriteLine("");
+                if (pokemon.is_default == true)
+                    Console.Write($"Pokémon inicial: Sim");
+                else
+                    Console.WriteLine($"Pokémon inicial: Não");
+                Console.WriteLine();
+                Console.WriteLine($"Peso do Pokémon:{pokemon.weight} kilos");
+
+
+                #region MELHORAR E ADAPTAR ESSE CÓDIGO AQUI
+
+                //Console.WriteLine($"Encontro da área de localização: {pokemon.location_area_encounters}");
+
+                // ============================NOVO REQUEST PARA LOCALIZAÇÃO ====================================================== //
+
+                // ver essa parte depois!
+
+                //string newRequest = $"https://pokeapi.co/api/v2/pokemon/{pokemon.id}/encounters";
+
+
+                //HttpWebRequest req2 = (HttpWebRequest)WebRequest.Create(newRequest);
+                //HttpWebResponse response2 = (HttpWebResponse)req2.GetResponse();
+                //HttpStatusCode status2 = response.StatusCode;
+
+                //Stream newDataStream = response2.GetResponseStream();
+                //StreamReader newReader = new StreamReader(newDataStream);
+                //string newResponseFromServer = newReader.ReadToEnd();
+
+                //Localizacao[] loc = JsonConvert.DeserializeObject<Localizacao[]>(newResponseFromServer);
+                //foreach (var item in loc)
+                //{
+                //    Console.WriteLine($"localização do pokemon:{item.url}");
+                //}
+
+
+                #endregion
+
+            }
+
+
+        }
+    }
+}
